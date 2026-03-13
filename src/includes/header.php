fab7c377a894f00956bdf9b00bb95fa43e8eac71
e5742u <felipe.alvariza-billar9@etu.univ-lorraine.fr>
2026-03-14 00:09:06 +0100

    Header provisoirement finalisé avec style bonne espacement logo validé par client et animation

--- src/includes/header.php
+++ src/includes/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>IEB - Intérieur Extérieur Bois</title>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
@@ -12,25 +12,22 @@
         <div class="container">
             <div class="logo">
                 <a href="index.php">
-                    <div class="logo-wrapper">
-                        <span class="brand">IEB</span>
-                        </div>
-                    <span class="sub-brand">Intérieur Extérieur Bois</span>
+                    <img src="assets/img/logo_ieb.png" alt="IEB - Intérieur Extérieur Bois">
                 </a>
             </div>
 
             <nav class="nav-menu">
                 <ul>
-                    <li><a href="services.php">Cuisines</a></li>
-                    <li><a href="services.php">Escaliers</a></li>
-                    <li><a href="realisations.php">Mobilier sur-mesure</a></li>
-                    <li><a href="services.php">Extérieur</a></li>
-                    <li><a href="entreprise.php">Atelier</a></li>
+                    <li><a href="index.php">Accueil</a></li>
+                    <li><a href="services.php">Nos Services</a></li>
+                    <li><a href="realisations.php">Réalisations</a></li>
+                    <li><a href="entreprise.php">L'Atelier</a></li>
+                    <li><a href="avis.php">Avis</a></li>
                 </ul>
             </nav>
 
             <div class="header-actions">
-                <a href="contact.php" class="btn-contact-pill">CONTACT</a>
+                <a href="contact.php" class="btn-contact-pill">Contact & Devis</a>
             </div>
         </div>
     </header>
